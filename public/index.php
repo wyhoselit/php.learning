cbe18cd9d5e456d62a056c690882cb45ec1a7d54
WYH osElit <wyh.oselit@gmail.com>
2017-06-04 19:51:37 +0800

    Add try error for debug in kernel session

--- public/index.php
+++ public/index.php
@@ -48,10 +48,15 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 */
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
+try {
+  $response = $kernel->handle(
+      $request = Illuminate\Http\Request::capture()
+  );
+} catch(\Exception $e) {
+    echo "<pre>";
+    echo $e;
+    echo "</pre>";
+}
 
 $response->send();
 
