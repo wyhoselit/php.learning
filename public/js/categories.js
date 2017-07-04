545da4617a0cfb44e64d1ff975c46c856d8d6b1d
WYH osElit <wyh.oselit@gmail.com>
2017-07-04 21:44:01 +0800

    New Category

--- public/js/categories.js
+++ public/js/categories.js
@@ -16,45 +16,46 @@ function createNewCategory(event) {
   }
   ajax("POST", "/admin/blog/category/create", "name=" + name, newCategoryCreated, [name]);
 }
-function newCategoryCreated(params, success, responseObj){
+
+function newCategoryCreated(params, success, responseObj) {
   location.reload();
 }
+
 function ajax(method, url, params, callback, callbackParams) {
   var http;
 
-    if (window.XMLHttpRequest) {
-      http = new XMLHttpRequest();
-    } else {
-      http = new ActiveXObject("Microsoft.XMLHTTP");
+  if (window.XMLHttpRequest) {
+    http = new XMLHttpRequest();
+  } else {
+    http = new ActiveXObject("Microsoft.XMLHTTP");
+  }
+
+  http.onreadystatechange = function() {
+    var obj;
+    if (http.readyState == XMLHttpRequest.DONE) {
+
+      if (http.status == 200) {
+        obj = JSON.parse(http.responseText);
+        callback(callbackParams, true, obj);
+      } else if (http.status == 400) {
+        alert('Category could not be saved. Please try again.');
+        callback(callbackParams, false);
+      } else {
+        obj = JSON.parse(http.responseText);
+        if (obj.message) {
+          alert(obj.message);
+        } else if (obj.name) {
+          alert(obj.name);
+        } else {
+          alert("please check the name");
+        }
+      }
     }
-
-        http.onreadystatechange = function() {
-          var obj;
-          if(http.readyState == XMLHttpRequest.DONE){
-
-            if(http.status == 200){
-              obj = JSON.parse(http.responseText);
-              callback(callbackParams, true, obj);
-            }else if(http.status == 400){
-              alert('Category could not be saved. Please try again.');
-              callback(callbackParams, false);
-            } else {
-              obj = JSON.parse(http.responseText);
-              if(obj.message){
-                alert(obj.message);
-              }else if(obj.name){
-                alert(obj.name);
-              }
-              else{
-                alert("please check the name");
-              }
-            }
-          }
-        };
-        http.open(method, baseUrl + url , true);
-        http.setRequestHeader('Content-Type','application/x-www-form-urlencoded');
-        http.setRequestHeader('X-Requested-With','XMLHttpRequest');
-        console.log(params);
-        http.send(params + "&_token="+token);
+  };
+  http.open(method, baseUrl + url, true);
+  http.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  http.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  console.log(params);
+  http.send(params + "&_token=" + token);
 
 }
